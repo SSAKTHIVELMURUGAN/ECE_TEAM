9c6be002f957c2bb306327b470bb4845b2900d1d
SAKTHIVEL MURUGAN S <129778327+SSAKTHIVELMURUGAN@users.noreply.github.com>
2024-04-13 09:13:38 +0530

    this is my updated code

--- excel1.py
+++ excel1.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 def read_csv_file(file_path):
@@ -5,7 +6,7 @@ def read_csv_file(file_path):
     return df
 
 def find_top_three_rank_holders(df):
-    top_three = df.sort_values(by='Rank').head(3)
+    top_three = df.sort_values(by='CGPA', ascending=False).head(3)
     return top_three
 
 def write_to_text_file(top_three, output_file):
@@ -14,8 +15,19 @@ def write_to_text_file(top_three, output_file):
         file.write(top_three.to_string(index=False))
 
 def main():
-    file_path = r"C:\Users\21ecb\Downloads\sample.csv"
+    directory = r"C:\Users\21ecb\Downloads"
+    
+    files = os.listdir(directory)
+    
+    csv_files = [file for file in files if file.endswith('.csv')]
+    
+    if len(csv_files) == 0:
+        print("No CSV files found in the directory.")
+        return
+    
+    file_path = os.path.join(directory, csv_files[0])
 
+    # Read CSV
     df = read_csv_file(file_path)
     
     if df is not None:
